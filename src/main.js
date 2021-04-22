057cf2d7aa1cc786f06530de478f3b20e1b5db9c
oshadian <oshadian@ucsc.edu>
2021-04-21 22:37:41 -0700

    implemented mouse movement/click to fire

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+/*
+ * Oran Shadian, Rocket Patrol Mod
+ * 4/21/21, 15 hours
+ * 
+ * Track a high score that persists across scenes and display it in the UI                      (5)
+ * Implement the speed increase that happens after 30 seconds in the original game              (5)
+ * Randomize each spaceship's movement direction at the start of each play                      (5)
+ * Allow the player to control the Rocket after it's fired                                      (5)
+ * Display the time remaining (in seconds) on the screen                                        (10)
+ * Create a new title screen (e.g., new artwork, typography, layout) (not really sure)          (10)
+ * Implement mouse control for player movement and mouse click to fire (20)
+ * Implement a new timing/scoring mechanism that adds time to the clock for successful hits (SP)(20)
+ * Implement a simultaneous two-player mode                                                     (30)
+ * 
+ * 
+ * 
+ * 
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
@@ -13,7 +32,7 @@ let borderPadding = borderUISize / 3;
 
 // keyboard key variables
 // controls
-let keyLEFT, keyRIGHT, keyUP, keyA, keyD, keyW, keyR;
+let keyLEFT, keyRIGHT, keyUP, keyA, keyD, keyW, keyR, keySPACE, ptr;
 
 // mode selection
 let key1, key2, keyESC;
@@ -72,4 +91,4 @@ let colorCodes =
 let colorP1, colorP2;
 
 // variables for gameplay
-let timeRemaining, scoreConfig;
+let timeRemaining, scoreConfig, highscore = 0;
