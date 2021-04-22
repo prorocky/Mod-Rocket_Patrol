b6e3aced6ddd0cd5be3a4c6d94eb48a43d901a14
oshadian <oshadian@ucsc.edu>
2021-04-21 22:44:45 -0700

    edited aboutModes scene

--- src/scenes/aboutModes.js
+++ src/scenes/aboutModes.js
@@ -21,7 +21,7 @@ class aboutModes extends Phaser.Scene {
             fixedWidth: 0
         }
         // about singleplayer
-        this.add.text(game.config.width / 2, game.config.height / 5, ' In singleplayer, you control your ship using \n (A) and (D) to move left/right and (W) to fire.\n If you press (SPACE) you can use your mouse \n to control your ship\'s horizontal movement. \n Then press (SPACE) again to change back.', textConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 5, ' In singleplayer, you control your ship using \n (A) and (D) to move left/right and (W) to fire.\n If you press (SPACE) you can use your mouse \n to control your ship\'s horizontal movement \n and click the mouse to fire. Then press (SPACE) \n again to change back to keyboard controls.', textConfig).setOrigin(0.5);
 
         // about multiplayer
         this.add.text(game.config.width / 2, game.config.height / 2, ' In multiplayer, P1 controls their ship using \n (A) and (D) to move left/right and (W) to fire \n and P2 controls their ship using (←) and (→)\n to move left/right. Timer will not be extended\n when destroying an enemy ship.', textConfig).setOrigin(0.5);
