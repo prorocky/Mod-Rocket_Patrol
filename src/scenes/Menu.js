fc8332f41f42490c9224700eec6d742ab987e0cb
oshadian <oshadian@ucsc.edu>
2021-04-24 12:54:56 -0700

    changed menu text

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -29,7 +29,7 @@ class Menu extends Phaser.Scene {
         // show menu text
         this.add.text(game.config.width / 2, borderUISize + borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#F3B141';
-        this.add.text(game.config.width / 2, game.config.height / 2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2, 'Use (A) (D) to move & (W) to fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
         this.add.text(game.config.width / 2, game.config.height / 2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
